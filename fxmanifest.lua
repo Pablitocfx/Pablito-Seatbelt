4a70ce75f5e4e7da17d9f68bc05623c3d1749b6b
Pablitocfx <julianxchavez24@gmail.com>
2025-02-06 00:02:14 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fxversion 'cerulean'
+game 'gta5'
+
+author 'Pablito'
+description 'A simple script for seatbelts'
+version '1.0.0'
+
+
+
+client_scripts {
+    'resource/cl.lua',
+}
+
+server_scripts {
+    'resource/sv.lua',
+}
+
+shared_script 'config.lua'
